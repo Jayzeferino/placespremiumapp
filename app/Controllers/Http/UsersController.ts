5d4e8ecc6586817c7dcea90a8cc502e35cc9bdce
jayzeferino <capelapop@gmail.com>
2021-09-05 21:25:41 -0300

    models funcionais

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,10 +1,12 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Encryption from '@ioc:Adonis/Core/Encryption'
 import User from 'App/Models/User';
 
 export default class UsersController {
-    public async create({ request }: HttpContextContract) {
+    public async create({ request }: HttpContextContract) { 
         const { name, password, email } = request.only(["name", "password", "email"]);
-        
+
+        const pw = Encryption.encrypt(password)
         const user = await User.create(
             {
                 name,
@@ -12,6 +14,8 @@ export default class UsersController {
                password,
             }
         );
-        return user
+        console.log("hash",pw);
+        return user;
+        
       }
 }
